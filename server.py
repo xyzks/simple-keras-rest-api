5819c6fd96bd9ce52a55ef02e516e7537d9ac621
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-06-19 23:48:38 -0300

    Supporting an image check of existance on pair

--- server.py
+++ server.py
@@ -37,28 +37,28 @@ def predict(request):
 
 	# ensure an image was properly uploaded to our endpoint
 	if request.method == "POST":
-		# if request.POST["image"]:
-		# read the image in PIL format
-		image = request.POST["image"].file.read()
-		image = Image.open(io.BytesIO(image))
+		if request.POST.__contains__("image"):
+			# read the image in PIL format
+			image = request.POST["image"].file.read()
+			image = Image.open(io.BytesIO(image))
 
-		# preprocess the image and prepare it for classification
-		image = prepare_image(image, target=(224, 224))
+			# preprocess the image and prepare it for classification
+			image = prepare_image(image, target=(224, 224))
 
-		# classify the input image and then initialize the list
-		# of predictions to return to the client
-		preds = model.predict(image)
-		results = imagenet_utils.decode_predictions(preds)
-		data["predictions"] = []
+			# classify the input image and then initialize the list
+			# of predictions to return to the client
+			preds = model.predict(image)
+			results = imagenet_utils.decode_predictions(preds)
+			data["predictions"] = []
 
-		# loop over the results and add them to the list of
-		# returned predictions
-		for (imagenetID, label, prob) in results[0]:
-			r = {"label": label, "probability": float(prob)}
-			data["predictions"].append(r)
+			# loop over the results and add them to the list of
+			# returned predictions
+			for (imagenetID, label, prob) in results[0]:
+				r = {"label": label, "probability": float(prob)}
+				data["predictions"].append(r)
 
-		# indicate that the request was a success
-		data["success"] = True
+			# indicate that the request was a success
+			data["success"] = True
 
 	# return the data dictionary as a JSON response
 	return data
