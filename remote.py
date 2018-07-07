df7334aceabd4703355905f8a0dd5d4b2b772670
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-07-07 16:06:42 -0300

    All settled for a specific condition

--- remote.py
+++ remote.py
@@ -4,4 +4,5 @@ def fn_default_url(sub):
 		DEFAULT_URL = '%s/predict' % sub.environ['URL']
 	if not 'URL' in sub.environ:
 		DEFAULT_URL='http://localhost:6543/predict'
+		os.environ['URL'] = DEFAULT_URL
 	return DEFAULT_URL
